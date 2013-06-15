26c8f683fabd9340d18d1aaed62af0b84d87bfdd
Lucas Stadler <mi.au@papill0n.org>
2013-06-15 12:55:34 +0200

    More tiny. (159 chars, hello C)
    If I'd take out the font-size, it would be tweetable.

--- speedr.js
+++ speedr.js
@@ -1,1 +1,1 @@
-d=document;w=d.body.textContent.split(/\s+/);p=0;n=function(){if(p<w.length){d.body.innerHTML="<h1 style='font-size:60pt'>"+w[p];p++}};window.setInterval(n,150)
+d=document;w=d.body.textContent.split(/\s+/);p=0;n=function(){if(p<w.length){d.body.innerHTML="<h1 style='font-size:60pt'>"+w[p++]}};window.setInterval(n,150)
