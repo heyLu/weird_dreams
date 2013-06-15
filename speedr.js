bc31dfd2e1d971873fe70a65e58602db3ad888cc
Lucas Stadler <mi.au@papill0n.org>
2013-06-15 13:09:29 +0200

    There's more. (145c, ternary if)

--- speedr.js
+++ speedr.js
@@ -1,1 +1,1 @@
-d=document;w=d.body.textContent.split(/\s+/);p=0;setInterval(function(){if(p<w.length){d.body.innerHTML="<h1 style='font-size:60pt'>"+w[p++]}},150)
+d=document;w=d.body.textContent.split(/\s+/);p=0;setInterval(function(){p<w.length?d.body.innerHTML="<h1 style='font-size:60pt'>"+w[p++]:0},150)
