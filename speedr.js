ded7f1560e4429b445f1d35ab81315f8eff265c1
Lucas Stadler <mi.au@papill0n.org>
2013-06-15 14:58:06 +0200

    Tweetable! (140c, aliased body instead of document)

--- speedr.js
+++ speedr.js
@@ -1,1 +1,1 @@
-d=document;w=d.body.textContent.split(/\s+/);p=0;setInterval(function(){p<w.length?d.body.innerHTML="<h1 style='font-size:60pt'>"+w[p++]:0},150)
+b=document.body;w=b.textContent.split(/\s+/);p=0;setInterval(function(){p<w.length?b.innerHTML="<h1 style='font-size:60pt'>"+w[p++]:0},150)
