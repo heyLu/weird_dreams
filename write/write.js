ddbe4c8629868f3d227528607df2e4b0c531cce4
Lucas Stadler <lu@papill0n.org>
2015-08-28 19:27:11 +0200

    Enable some neat input rules
    These provide a few more shortcuts that we can use.  For example, hashes
    at the start of a line make headings now.

--- write/write.js
+++ write/write.js
@@ -1,7 +1,9 @@
 import {ProseMirror} from "./prosemirror-dist/edit"
+import "./prosemirror-dist/inputrules/autoinput"
 
 let pm = window.pm = new ProseMirror({
 	place: document.querySelector("#editor"),
+	autoInput: true,
 	doc: document.querySelector("#content"),
 	docFormat: "dom"
 });
