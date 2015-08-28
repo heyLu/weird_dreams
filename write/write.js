e6648dc2f6753913fd9e5688876d22714e504cd8
Lucas Stadler <lu@papill0n.org>
2015-08-28 23:51:49 +0200

    Add input rules for inline code and emphasis
    It may not look like much, but this has cost me several hours, most of
    which were spent looking through the ProseMirror code and figuring out
    why it threw away the styles I passed in.  As it turns out use
    `.insert`, not `.insertInline`, if you want to keep the styles intact.
    Who knew...
    
    Next up: bold.  (And maybe a better focus mode, i.e. sentence oriented.)

--- write/write.js
+++ write/write.js
@@ -1,5 +1,21 @@
 import {ProseMirror} from "./prosemirror-dist/edit"
+import {Span, style} from "./prosemirror-dist/model"
+import {addInputRules, Rule} from "./prosemirror-dist/inputrules/inputrules"
 import "./prosemirror-dist/inputrules/autoinput"
+
+function wrapInline(pm, match, pos, style) {
+	let start = pos.shift(-(match[0].length));
+	let text = match[0].substring(1, match[0].length - 1);
+	let span = Span.text(text, [style]);
+	pm.apply(pm.tr.delete(start, pos).insert(start, span));
+	pm.setInlineStyle(style);
+}
+
+let additionalRules = [
+	new Rule("`", /\`\w+(?: \w+)*\`/, (pm, match, pos) => wrapInline(pm, match, pos, style.code)),
+	new Rule("*", /\*\w+(?: \w+)*\*/, (pm, match, pos) => wrapInline(pm, match, pos, style.em)),
+	new Rule("_", /_\w+(?: \w+)*_/, (pm, match, pos) => wrapInline(pm, match, pos, style.em))
+];
 
 let pm = window.pm = new ProseMirror({
 	place: document.querySelector("#editor"),
@@ -7,3 +23,4 @@ let pm = window.pm = new ProseMirror({
 	doc: document.querySelector("#content"),
 	docFormat: "dom"
 });
+addInputRules(pm, additionalRules);
